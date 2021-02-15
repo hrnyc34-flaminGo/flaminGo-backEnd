a4fa9e05960de118825bb58ee729274140af4ed4
Colin Chauche <colin.chauche@gmail.com>
2021-02-15 15:58:29 -0500

    Add first draft of employee schema

--- db/schemas/employees.js
+++ db/schemas/employees.js
@@ -4,9 +4,51 @@ db.createCollection('employees', {
       bsonType: 'object',
       required: [],
       properties: {
-        'property': {
-          bsonType: 'int',
+        'firstName': {
+          bsonType: 'string',
         },
+        'lastName': {
+          bsonType: 'string'
+        },
+        'address1': {
+          bsonType: 'string'
+        },
+        'address2': {
+          bsonType: 'string'
+        },
+        'city': {
+          bsonType: 'string'
+        },
+        'state': {
+          bsonType: 'string'
+        },
+        'zipcode': {
+          bsonType: 'string'
+        },
+        'wage': {
+          bsonType: 'decimal'
+        },
+        'startDate': {
+          bsonType: 'date'
+        },
+        'username': {
+          bsonType: 'string'
+        },
+        'passwordHash': {
+          bsonType: 'string'
+        },
+        'needsNewPassword': {
+          bsonType: 'boolean'
+        },
+        'salt': {
+          bsonType: 'string'
+        },
+        'position': {
+          bsonType: 'string'
+        },
+        'isActive': {
+          bsonType: 'boolean'
+        }
       }
     }
   }
