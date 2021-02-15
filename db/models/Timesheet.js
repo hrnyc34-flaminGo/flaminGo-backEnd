5267f5bf533c2ad2dfbf47586ea011fd5f876c03
William Chang <wdchang86@gmail.com>
2021-02-15 16:03:40 -0500

    Completed Timesheet routes.

--- db/models/Timesheet.js
+++ db/models/Timesheet.js
@@ -3,16 +3,17 @@ const { Schema } = mongoose;
 const db = require('../../db');
 
 const timesheetSchema = new Schema({
-  employee_id: String,
-  monday: Number,
-  tuesday: Number,
-  wednesday: Number,
-  thursday: Number,
-  friday: Number,
-  saturday: Number,
-  sunday: Number,
-  weekStart: String,
-  weekEnd: String
+  employee_id: {type: String, required: true},
+  monday: {type: Number, default: 0},
+  tuesday: {type: Number, default: 0},
+  wednesday: {type: Number, default: 0},
+  thursday: {type: Number, default: 0},
+  friday: {type: Number, default: 0},
+  saturday: {type: Number, default: 0},
+  sunday: {type: Number, default: 0},
+  weekStart: {type: String, default: null},
+  weekEnd: {type: String, default: null},
+  versionKey: false
 });
 
 const Timesheet = mongoose.model('Timesheet', timesheetSchema);
--- server/controllers/timesheets.js
+++ server/controllers/timesheets.js
@@ -1,9 +1,10 @@
-const Timesheet = require('../../../db/models/Timesheet.js');
+const Timesheet = require('../../db/models/Timesheet.js');
 
 module.exports = {
   get: (req, res) => {
+    const { employee_id } = req.params
     const count = req.query.count ? req.query.count : 0;
-    Timesheet.find(req.params.employee_id).sort({weekStart: 'desc'}).limit(count).exec()
+    Timesheet.find({employee_id}).sort({weekStart: 'desc'}).limit(Number(count)).exec()
       .then(result => {
         res.status(200).json(result);
       })
@@ -13,15 +14,24 @@ module.exports = {
   },
 
   addOrEdit: (req, res) => {
-    const {timesheet_id, employee_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday, weekStart, weekEnd} = req.body;
+    const {timesheet_id, employee_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday} = req.body;
 
-    Timesheet.findByIdAndUpdate( timesheet_id, { ...(employee_id && {employee_id}), ...(monday && {monday}), ...(tuesday && {tuesday}), ...(wednesday && {wednesday}), ...(thursday && {thursday}), ...(friday && {friday}), ...(saturday && {saturday}), ...(sunday && {sunday}), ...(weekStart && {weekStart}), ...(weekEnd && {weekEnd}) }).exec()
+    const curr = new Date; // get current date
+    const start = curr.getDate() - curr.getDay() + 1; // first day is Monday
+    const end = start + 6; // last day is Sunday
+    const weekStart = new Date(curr.setDate(start)).toISOString().slice(0, 10); //"2021-02-15"
+    const weekEnd = new Date(curr.setDate(end)).toISOString().slice(0, 10); //"2021-02-21"
+
+    const update = {...(monday && {monday}), ...(tuesday && {tuesday}), ...(wednesday && {wednesday}), ...(thursday && {thursday}), ...(friday && {friday}), ...(saturday && {saturday}), ...(sunday && {sunday}), ...(weekStart && {weekStart}), ...(weekEnd && {weekEnd}) }
+
+    const options = {upsert: true, new: true}
+
+    Timesheet.findOneAndUpdate({employee_id, weekStart}, update, options).exec()
       .then(result => {
-        res.status(200).json(result);
+        res.status(201).json(result);
       })
       .catch(err => {
         res.sendStatus(500);
       })
   }
-
-};
+}
