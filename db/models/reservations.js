31265301ae6e97eabe39f8a0b63ff52d5654ab87
Colin Chauche <colin.chauche@gmail.com>
2021-02-16 14:01:33 -0500

    Create reservationSchema using mongoose

---
+++ db/models/reservations.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const { Schema, ObjectId } = mongoose;
+const db = require('../../db');
+
+const reservationsSchema = new Schema({
+  bookingGuest: {type: Object, required: true},
+  'room_id': {type: ObjectId},
+  'roomType_id': {type: ObjectId, required: true},
+  checkIn: {type: Date, required: true},
+  checkOut: {type: Date, required: true},
+  guestList: {type: Array, default: []},
+  totalCost: {type: Number, default: 0}
+}, {
+  versionKey: false
+});
+
+const Reservation = mongoose.model('Reservation', reservationsSchema);
+
+module.exports = Reservation;
