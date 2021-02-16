0822dbcbd441d62bf06314e034e1c14caa5fc1bd
William Chang <wdchang86@gmail.com>
2021-02-15 23:05:38 -0500

    created taskSchema

--- db/models/Task.js
+++ db/models/Task.js
@@ -1,3 +1,25 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const db = require('../../db');
+
+const taskSchema = new Schema({
+  room_id: {type: Schema.Types.ObjectId, ref: 'Room'},
+  roomNumber: String,
+  location: String,
+  department: {type: String, required: true},
+  taskTitle: {type: String, required: true},
+  taskDescription: {type: String, required: true},
+  employeeCreated: {type: Schema.Types.ObjectId, ref: 'Employee'},
+  employeeCompleted: {type: Schema.Types.ObjectId, ref: 'Employee'},
+  dueBy: {type: String, required: true},
+  isCompleted: {type: Boolean, default: false},
+  completedAt: {type: String, default: null}
+},
+{
+  timestamps: true,
+  versionKey: false
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
--- server/controllers/tasks.js
+++ server/controllers/tasks.js
@@ -1,8 +1,9 @@
-const Example = require('../../db/models/example.js');
+const Task = require('../../db/models/Task.js');
 
 module.exports = {
   get: (req, res) => {
-
+    const {room_id, roomNumber, isComplete, location, dueBy} = req.query;
+    const query = {...(room_id && {room_id}), ...(roomNumber && {roomNumber}), ...(isComplete && {isComplete}), ...(location && {location}), ...(dueBy && {dueBy})}
   },
   post: (req, res) => {
 
