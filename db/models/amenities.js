1530b69ea0848c59798ab0752db45fcd82589f4a
claire <kimsujin8702@gmail.com>
2021-02-15 20:01:44 -0500

    Write first  draft for amenities schema

--- db/models/amenities.js
+++ db/models/amenities.js
@@ -2,11 +2,24 @@ var mongoose = require('mongoose');
 var db = require('../../db');
 
 const amenitiesSchema = mongoose.Schema({
-  amenity: String
+  amenity: { type: String, unique: true }
 });
-
 
 const Amenities = mongoose.model('Amenities', amenitiesSchema);
 
 module.exports = {
+  readAll: () => {
+    return Amenities.find().exec();
+  },
+  readOne: ( id )=>{
+    return Amenities.findOne({ _id: id }).exec();
+  },
+  create: (type) => {
+    return Amenities.create({
+      amenity: type
+    });
+  },
+  deleteOne: ( id )=>{
+    return Amenities.deleteOne( { _id: id });
+  },
 };
