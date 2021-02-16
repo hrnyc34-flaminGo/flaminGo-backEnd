a6392d208c9470e3865c736a306e72009b53a9da
claire <kimsujin8702@gmail.com>
2021-02-16 02:26:08 -0500

    Write first draft Schema / Model for Rooms

--- server/controllers/rooms.js
+++ server/controllers/rooms.js
@@ -1,115 +1,110 @@
-const Example = require('../../db/models/example.js');
+var ObjectId = require('mongoose').Types.ObjectId;
+
+const roomsMethod = require('../../db/models/rooms.js');
+const amenitiesMethod = require('../../db/models/amenities.js');
+const roomTypeMethod = require('../../db/models/roomTypes.js');
 
 module.exports = {
   get: (req, res) => {
-    // GET /rooms Retrieves a list of rooms. By default all rooms are returned
-    // GET /rooms/:room_id Retrieves a specific room by its id
+    if (req.url === '/') {
+      const { room_id } = req.params;
+      let getRoomId = new ObjectId(room_id);
+      if (!room_id) {
+        roomsMethod.read()
+          .then(result => res.status(200).json(result))
+          .catch(err => {
+            res.sendStatus(404);
+            console.log('GET /rooms', err);
+          });
+      } else {
+        // roomsMethod.readOne(getRoomId)
+        //   .then(result => res.status(200).json(result))
+        //   .catch(err => {
+        //     res.sendStatus(404);
+        //     console.log('GET /rooms/:rood_id', err);
+        //   });
+      }
+    } else if (req.url === '/amenities') {
+      amenitiesMethod.readAll()
+        .then(result => res.status(200).json(result))
+        .catch(err => {
+          res.sendStatus(404);
+          console.log('GET_ /rooms/amenities_error', err);
+        });
 
-    // [
-    //   {
-    //     "_id": "507c7f79bcf86cd7994f6c0e",
-    //     "roomNumber": "110",
-    //     "floorNumber": 1,
-    //     "roomType": "Double Queen",
-    //     "price": 150.00,
-    //     "amenities": [
-    //       "Non-Smoking"
-    //     ],
-    //     "isClean":true,
-    //     "isOccupied": true,
-    //     "isUsable": true,
-    //     "currentGuests": [
-    //       "Bobby Jr Palmer"
-    //     ],
-    //     "tasks": [
-    //       {
-    //         "_id":"5febcfb988e5d76e417427c6",
-    //         "taskTitle": "Daily cleaning",
-    //         "department":"Housekeeping"
-    //       }
-    //     ]
-    //   }
-
-    // GET /rooms/amenities Retrieves a list of all room amenities(Status: 200 OK)
-
-    // [
-    //   {
-    //     "_id": "5ff90d07450a752b55cbf9fc",
-    //     "amenity":"Fridge"
-    //   },
-    //   {
-    //     "_id": "5ff90d07450a752b55cbf9fc",
-    //     "amenity":"TV"
-    //   },
-    //   {
-    //     "_id": "5ff90d07450a752b55cbf9fc",
-    //     "amenity":"Handicapped Shower"
-    //   },
-    //   {
-    //     "_id": "5ff90d07450a752b55cbf9fc",
-    //     "amenity":"Non-Smoking"
-    //   },
-    //   ...
-    // ]
-
-    // GET /rooms/types Retrieves a list of all room types
-
-    // [
-    //   {
-    //     "_id": "5ff8c7b6aa12892093205486",
-    //     "roomType": "Single Queen",
-    //     "price": 150.00,
-    //   },
-    //   {
-    //     "_id": "5ff8c7b6aa12892093205486",
-    //     "roomType": "Single King",
-    //     "price": 200.00
-    //   },
-    //   {
-    //     "_id": "5ff8c7b6aa12892093205486",
-    //     "roomType": "Suite",
-    //     "price": 400.00
-    //   },
-    //   {
-    //     "_id": "5ff8c7b6aa12892093205486",
-    //     "roomType": "Double Twin",
-    //     "price": 100.00
-    //   },
-    //   ...
-    // ]
+    } else if (req.url === '/types') {
+      roomTypeMethod.readAll()
+        .then(result => res.status(200).json(result))
+        .catch(err => {
+          res.sendStatus(404);
+          console.log('GET_ /rooms/types_error', err);
+        });
+    }
 
   },
   post: (req, res) => {
-    // POST /rooms Add a new room to the room list
 
-    // {
-    //   "_id": "507c7f79bcf86cd7994f6c0e",
-    //   "roomNumber": "110",
-    //   "floorNumber": 1,
-    //   "roomType": "Double Queen",
-    //   "price": 150.00,
-    //   "amenities": [
-    //     "Non-Smoking",
-    //     "Pool Side",
-    //     "Mini-Fridge"
-    //   ]
-    // }
+    if (req.url === '/') {
+      let roomInfo = req.body;
 
+      roomTypeMethod.readOne(req.body.roomType)
+        .then(result => {
+          roomInfo['price'] = result.price;
+          roomInfo['amenities'] = result.amenities;
+          // ADD tasks
+
+          roomsMethod.update(roomInfo)
+            .then((result) => {
+              res.sendStatus(201);
+            });
+        })
+        .catch((err) => {
+          res.sendStatus(404);
+          console.error('POST /rooms Add ERROR', err);
+        });
+
+    } else if (req.url === '/amenities') {
+      amenitiesMethod.create(req.body.amenity)
+        .then((result) => {
+          res.sendStatus(201);
+        })
+        .catch((err) => {
+          res.sendStatus(404);
+          console.error('PUT_ /rooms/amenities_Error', err);
+        });
+
+    } else if (req.url === '/types') {
+      roomTypeMethod.update(req.body)
+        .then((result) => {
+          res.sendStatus(201);
+        })
+        .catch((err) => {
+          res.sendStatus(404);
+          console.error('PUT_ /rooms/types_Error', err);
+        });
+    }
   },
   put: (req, res) => {
-    // PUT /rooms/:room_id
+    console.log('req.params', req.params);
+    console.log('req.body', req.body);
+    const { room_id } = req.params;
+    let roomInfo = req.body;
+    roomInfo['_id'] = new ObjectId(room_id);
 
-    // {
-    //   "_id": "507c7f79bcf86cd7994f6c0e",
-    //   "roomNumber": "110",
-    //   "floorNumber": 1,
-    //   "roomType": "Double Queen",
-    //   "amenities": [
-    //     "Non-Smoking",
-    //     "Pool Side",
-    //     "Mini-Fridge"
-    //   ]
-    // }
+    roomTypeMethod.readOne(req.body.roomType)
+      .then(result => {
+        roomInfo['price'] = result.price;
+        roomInfo['amenities'] = result.amenities;
+        // ADD tasks HERE
+        roomsMethod.update(roomInfo)
+          .then((result) => {
+            res.sendStatus(201);
+          });
+      })
+      .catch((err) => {
+        res.sendStatus(404);
+        console.error('PUT /rooms Edit ERROR', err);
+      });
   },
   delete: (req, res) => {
   }
