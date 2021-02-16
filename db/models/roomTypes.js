33052a53670eeb67baad27440e9f07b4cebe60b0
claire <kimsujin8702@gmail.com>
2021-02-15 21:14:45 -0500

    Write first draft for roomType schema / model

--- db/models/roomTypes.js
+++ db/models/roomTypes.js
@@ -1,13 +1,39 @@
-var mongoose = require('mongoose');
-var db = require('../../db');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const db = require('../../db');
 
-const roomTypesSchema = mongoose.Schema({
-  _id: { type: Number, unique: true },
+const Amenities = require('../../db/models/amenities.js');
+
+const roomTypesSchema = new Schema({
   roomType: { type: String, unique: true },
-  price: Number
+  price: Number,
+  amenities: []
 });
 
 const RoomTypes = mongoose.model('RoomTypes', roomTypesSchema);
 
-module.exports = {
+let roomTypeMethod = {
+  readAll: ( type ) => {
+    return RoomTypes.find().exec();
+  },
+  readOne: ( type ) => {
+    return RoomTypes.findOne({ roomType: type }).exec();
+  },
+  update: (one) => {
+    return RoomTypes.updateMany(
+      { roomType: one.roomType },
+      {
+        roomType: one.roomType,
+        price: one.price,
+        amenities: one.amenities,
+      },
+      { upsert: true }
+    );
+  },
+  deleteOne: (type) => {
+    return RoomTypes.deleteOne({ roomType: type });
+  },
 };
+
+module.exports = RoomTypes;
+module.exports = roomTypeMethod;
