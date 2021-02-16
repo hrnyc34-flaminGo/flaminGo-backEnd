3cef4f614df90285cbcf7b7c3ba18970675bfa8b
claire <kimsujin8702@gmail.com>
2021-02-16 02:36:10 -0500

    Revert "Write first draft Schema / Model for Rooms"
    This reverts commit a6392d208c9470e3865c736a306e72009b53a9da.

--- db/models/rooms.js
+++ db/models/rooms.js
@@ -1,57 +1,29 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-const db = require('../../db');
+var mongoose = require('mongoose');
+var db = require('../../db');
 
-const RoomTypes = require('../../db/models/roomTypes.js');
+const roomsSchema = mongoose.Schema({
+  reservation_id: Number, // currentGuests: [],
+  tasks_id: Number, // tasks: [ {taskTitle: String, department: String} ]
 
-const roomsSchema = new Schema({
-  reservations_id: { type: Schema.Types.ObjectId, ref: 'Reservations', required: true },
-  roomNumber: { type: String, unique: true },
-  floorNumber: { type: Number },
-  roomType: { type: String },
-  price: { type: Number },
+  roomType_id: Number, // roomType: String, price: Number,
+  amenities_id: Number, // amenities: [],
+
+  roomNumber: String,
+  floorNumber: Number,
+
+  roomType: String,
+  price: Number,
+
   amenities: [],
-  isClean: { type: Boolean, default: false },
-  isOccupied: { type: Boolean, default: false },
-  isUsable: { type: Boolean, default: false },
+  isClean: Boolean,
+  isOccupied: Boolean,
+  isUsable: Boolean,
   currentGuests: [],
   tasks: []
-},
-{
-  versionKey: false
 });
+
 
 const Rooms = mongoose.model('Rooms', roomsSchema);
 
-let roomsMethod = {
-  read: () => {
-    return Rooms.find().exec();
-  },
-  readOne: (id) => {
-    return Rooms.findOne({ '_id': id }).exec();
-  },
-  update: (one) => {
-    return Rooms.updateMany(
-      { _id: one._id },
-      {
-        floorNumber: one.floorNumber,
-        roomNumber: one.roomNumber,
-        roomType: one.roomType,
-        price: one.price,
-        amenities: one.amenities,
-        isClean: one.isClean,
-        isOccupied: one.isOccupied,
-        isUsable: one.isUsable,
-        currentGuests: one.currentGuests,
-        tasks: one.tasks
-      },
-      { upsert: true }
-    );
-  },
-  deleteOne: (num) => {
-    return Rooms.deleteOne({ roomNumber: num });
-  },
+module.exports = {
 };
-
-module.exports = Rooms;
-module.exports = roomsMethod;
