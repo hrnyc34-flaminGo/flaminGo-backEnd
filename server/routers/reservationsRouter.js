1eaaff359f81ab9b169dd8dfb69b442a235ab729
dd0sxx <contacttheotelonis@gmail.com>
2021-02-17 14:37:53 -0500

    fixed merge conflict error, readjusted reservation cb function names

--- server/routers/reservationsRouter.js
+++ server/routers/reservationsRouter.js
@@ -5,10 +5,10 @@ const { reservationsRouterPermissions } = require('../middleware/authentication'
 // router.use(reservationsRouterPermissions);
 
 router.get('/', controller.reservations.get);
-router.get('/availability/:date', controller.reservations.get);
+router.get('/availability/:date', controller.reservations.getAvailibility);
 router.post('/', controller.reservations.post);
-router.put('/checkIn/reservation_id', controller.reservations.put);
-router.put('/checkOut/:reservations_id', controller.reservations.put);
+router.put('/checkIn/reservation_id', controller.reservations.checkIn);
+router.put('/checkOut/:reservations_id', controller.reservations.checkOut);
 // router.delete('/', controller.reservations.delete);
 
 module.exports = router;
