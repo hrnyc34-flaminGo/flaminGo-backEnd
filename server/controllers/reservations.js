a8cd3f4ea924ce357979182545527c600b465ca4
Colin Chauche <colin.chauche@gmail.com>
2021-02-16 20:59:19 -0500

    Search for name text working

--- server/controllers/reservations.js
+++ server/controllers/reservations.js
@@ -1,5 +1,6 @@
 const Reservation = require('../../db/models/reservations');
 const reformat = require('../helpers/reformat');
+const { ObjectId } = require('mongoose').Types;
 
 const formatReservation = (reservation) => {
   let bookingGuest = reformat.guestToName(reservation.bookingGuest);
@@ -7,7 +8,7 @@ const formatReservation = (reservation) => {
   let totalCost = reformat.decimal128ToFloat(reservation.totalCost);
   let checkIn = reformat.toDashDate(reservation.checkIn);
   let checkOut = reformat.toDashDate(reservation.checkOut);
-  let {_id, room_id, roomNumber = "", roomType} = reservation;
+  let { _id, room_id, roomNumber = '', roomType } = reservation;
   return {
     bookingGuest,
     guestList,
@@ -17,22 +18,30 @@ const formatReservation = (reservation) => {
     roomNumber,
     roomType,
     checkIn,
-    checkOut
+    checkOut,
   };
 };
 
 module.exports = {
   get: (req, res) => {
-    // Reservation.find().sort({checkIn: -1})
-    //   .then((result) => {
-    //     let body = result.map(formatReservation);
-    //     res.status(200).send(body);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //     res.sendStatus(500);
-    //   });
-    Reservation.searchReservations()
+    let {
+      firstName = '',
+      lastName = '',
+      checkIn,
+      checkOut,
+      reservation_id,
+    } = req.query;
+
+    const query = {};
+    if (firstName.length > 0 || lastName.length > 0) {
+      query.$text = { $search: firstName + ' ' + lastName };
+    }
+    if (checkIn) { query.checkIn = new Date(checkIn); }
+    if (checkOut) { query.checkOut = new Date(checkOut); }
+    if (reservation_id) { query.reservation_id = ObjectId(reservation_id); }
+    debugger;
+
+    Reservation.searchReservations(query)
       .then((result) => {
         let body = result.map(formatReservation);
         res.status(200).send(body);
@@ -42,13 +51,7 @@ module.exports = {
         res.sendStatus(500);
       });
   },
-  post: (req, res) => {
-
-  },
-  put: (req, res) => {
-
-  },
-  delete: (req, res) => {
-
-  }
+  post: (req, res) => {},
+  put: (req, res) => {},
+  delete: (req, res) => {},
 };
