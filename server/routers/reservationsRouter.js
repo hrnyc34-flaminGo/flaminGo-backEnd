76a5b0468606181075dc4663a6f0779572cdb952
dd0sxx <contacttheotelonis@gmail.com>
2021-02-17 16:11:35 -0500

    made minor fixes while testing routes

--- server/routers/reservationsRouter.js
+++ server/routers/reservationsRouter.js
@@ -1,5 +1,5 @@
-// const router = require('express').Router();
-// const controller = require('../controllers');
+const router = require('express').Router();
+const controller = require('../controllers');
 // const { reservationsRouterPermissions } = require('../middleware/authentication');
 
 // // router.use(reservationsRouterPermissions);
@@ -7,9 +7,9 @@
 router.get('/', controller.reservations.get);
 router.get('/availability/:date', controller.reservations.getAvailibility);
 router.post('/', controller.reservations.post);
-router.put('/checkIn/reservation_id', controller.reservations.checkIn);
+router.put('/checkIn/:reservation_id', controller.reservations.checkIn);
 router.put('/checkOut/:reservations_id', controller.reservations.checkOut);
 
 // router.delete('/', controller.reservations.delete);
 
-// module.exports = router;
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, '../../flaminGo-frontEnd/client/dist
 // app.use(checkJwt);
 
 app.use('/employees', employeesRouter);
-// app.use('/reservations', reservationsRouter);
+app.use('/reservations', reservationsRouter);
 app.use('/rooms', roomsRouter);
 app.use('/tasks', tasksRouter);
 app.use('/timesheets', timesheetsRouter);
