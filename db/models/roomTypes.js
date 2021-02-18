b1a969e0ffc172daf2e7058559a228543f0f5b19
claire <kimsujin8702@gmail.com>
2021-02-17 19:12:37 -0500

    update roomTypes roomType Method for decimal128

--- db/models/roomTypes.js
+++ db/models/roomTypes.js
@@ -39,32 +39,3 @@ module.exports = {
     }
   }
 };
-
-module.exports = {
-  RoomTypes: mongoose.model('RoomTypes', roomTypesSchema),
-
-  roomTypeMethod: {
-    readAll: () => {
-      return module.exports.RoomTypes.find().exec();
-    },
-    readOne: (type) => {
-      return module.exports.RoomTypes.findOne({ roomType: type }).exec();
-    },
-    update: (one) => {
-      let newPrice = decimal128ToMoneyString(one.price);
-
-      return module.exports.RoomTypes.updateMany(
-        { roomType: one.roomType },
-        {
-          roomType: one.roomType,
-          price: newPrice,
-          amenities: one.amenities,
-        },
-        { upsert: true }
-      );
-    },
-    deleteOne: (type) => {
-      return module.exports.RoomTypes.deleteOne({ roomType: type });
-    }
-  }
-};
