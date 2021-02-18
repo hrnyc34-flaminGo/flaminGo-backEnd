c787d215f2ca424936f3e1eaae9f9a1b2e5c6f3b
dd0sxx <contacttheotelonis@gmail.com>
2021-02-18 17:00:49 -0500

    fixed docker file and static bundle serve

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../../flaminGo-frontEnd/client/dist')));
+// app.use(express.static(path.join(__dirname, '../../flaminGo-frontEnd/client/dist')));
 // app.use(checkJwt);
 
 app.use('/employees', employeesRouter);
